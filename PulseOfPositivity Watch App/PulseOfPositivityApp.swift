1efafd7bc65084f8706a65e5a66672d3d266c797
Kirsten <ikirstenhd@gmail.com>
2025-08-23 21:14:16 +0100

    Initial Commit

---
+++ PulseOfPositivity Watch App/PulseOfPositivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseOfPositivityApp.swift
+//  PulseOfPositivity Watch App
+//
+//  Created by Kirsten Ali on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PulseOfPositivity_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulseOfPositivity Watch AppTests/PulseOfPositivity_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PulseOfPositivity_Watch_AppTests.swift
+//  PulseOfPositivity Watch AppTests
+//
+//  Created by Kirsten Ali on 23/08/2025.
+//
+
+import Testing
+@testable import PulseOfPositivity_Watch_App
+
+struct PulseOfPositivity_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
